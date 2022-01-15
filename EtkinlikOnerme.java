073e4fb6c9b61e9ac09186d47652aa97393410ab
Aslinur Unal <96949096+aslinurunal@users.noreply.github.com>
2022-01-16 01:09:03 +0300

    Add files via upload

---
+++ EtkinlikOnerme.java
@@ -0,0 +1,26 @@
+package javaBaslangic;
+
+import java.util.Scanner;
+
+public class EtkinlikOnerme {
+
+	public static void main(String[] args) {
+		int heat;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Sýcaklýk Giriniz: ");
+		heat = input.nextInt();
+		
+		if (heat < 5) {
+			System.out.println("Kayak yapabilirsiniz.");
+		}else if (heat >= 5 && heat < 15) {
+			System.out.println("Sinemaya gidebilirsiniz.");
+		}else if (heat >= 15 && heat < 25) {
+			System.out.println("Pikniðe gidebilirsiniz.");
+		}else {
+			System.out.println("Yüzmeye gidebilirsiniz.");
+		}
+
+	}
+
+}
