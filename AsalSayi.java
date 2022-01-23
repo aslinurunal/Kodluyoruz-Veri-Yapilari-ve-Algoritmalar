194a71978b25fb694f7e47dcb8342742c0770104
Aslinur Unal <96949096+aslinurunal@users.noreply.github.com>
2022-01-23 22:38:38 +0300

    Add files via upload

---
+++ AsalSayi.java
@@ -0,0 +1,21 @@
+package javaBaslangic;
+
+public class AsalSayi {
+
+	public static void main(String[] args) {
+		int count = 0;
+		for (int number = 2; number <= 100; number++) {
+			int control = 0;
+			for (int i = 2; i < number; i++) {
+				if (number % i == 0) {
+					control = 1;
+					break;
+				}
+			}
+			if (control == 0) {
+				System.out.print(number + ", ");
+				count++;
+			}
+		}
+	}
+}
