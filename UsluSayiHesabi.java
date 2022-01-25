4c2e3621e691f3a88f78753e757d28f520c9281b
Aslinur Unal <96949096+aslinurunal@users.noreply.github.com>
2022-01-25 22:31:01 +0300

    Add files via upload

---
+++ UsluSayiHesabi.java
@@ -0,0 +1,26 @@
+package javaBaslangic;
+
+import java.util.Scanner;
+
+public class UsluSayiHesabi {
+	static int power(int a, int b) {
+		int result = 1;
+		for (int i = 1; i <= b; i++) {
+			result *= a;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Taban deðeri giriniz: ");
+		int a = scan.nextInt();
+		System.out.print("Üs deðerini giriniz: ");
+		int b = scan.nextInt();
+
+		System.out.println("Sonuç: " + power(a, b));
+	}
+
+}
