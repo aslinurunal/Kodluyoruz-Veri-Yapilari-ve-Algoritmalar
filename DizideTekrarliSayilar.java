01fa997a58e092ebde5a8bc8efb3e7070cd5d109
Aslinur Unal <96949096+aslinurunal@users.noreply.github.com>
2022-02-18 21:49:19 +0300

    Add files via upload

---
+++ DizideTekrarliSayilar.java
@@ -0,0 +1,41 @@
+package javaBaslangic;
+
+public class DizideTekrarliSayilar {
+
+	static boolean isFind(int[] arr, int value) {
+		for (int i : arr) {
+			if (i == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		int[] list = { 3, 7, 3, 2, 9, 10, 10, 22, 2, 2 };
+
+		int[] duplicate = new int[list.length];
+
+		int startIndex = 0;
+
+		for (int i = 0; i < list.length; i++) {
+
+			for (int j = 0; j < list.length; j++) {
+
+				if ((list[i] % 2 == 0) && (i != j) && (list[i] == list[j])) {
+
+					if (!isFind(duplicate, list[i])) {
+						duplicate[startIndex++] = list[i];
+					}
+					break;
+				}
+			}
+		}
+		for (int value : duplicate) {
+			if (value != 0) {
+				System.out.print(value + " ");
+			}
+		}
+	}
+}
